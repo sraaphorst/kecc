574b929563500a607c65ec4165d21ef89780decc
Sebastian Raaphorst <sraaphorst@gmail.com>
2024-01-23 20:33:52 -1000

    Initial checkin.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,8 +8,8 @@ fun main() {
     println("Hello World!")
     val n = MPZ("44444666666888888889999999997")
     val ring = Zn(n)
-    val a = ring.pull(1000.toMPZ())
-    val b = ring.pull(2000.toMPZ())
+    val a = ring.EZn(1000.toMPZ())
+    val b = ring.EZn(2000.toMPZ())
 
     // Using mod functions.
     // md = a / b (mod n)
@@ -20,8 +20,15 @@ fun main() {
     // a = md * b (mod n)
     println(b * md)
 
-    val ring2 = Zn("13".toMPZ())
-    val c = ring2.pull(10.toMPZ())
-    val d = a * c
-    println(d)
+
+    val maxBound = 23L
+    println("Calculating square roots mod $maxBound")
+    val ring3 = Zn(maxBound.toMPZ())
+    (1 until maxBound).map { it.toMPZ() }.forEach {
+        val elem = ring3.EZn(it)
+        if (elem.sqrt != null) {
+            val s = elem.sqrt!!
+            println("$elem -> ${elem.sqrt} -> ${s.pow(2)} = ${s * s}")
+        }
+    }
 }
--- src/main/kotlin/mpz/MPZExtensions.kt
+++ src/main/kotlin/mpz/MPZExtensions.kt
@@ -1,18 +1,49 @@
 package mpz
 
 import it.unich.jgmp.MPZ
+import it.unich.jgmp.RandState
 import it.unich.jgmp.MPZ.PrimalityStatus
+import java.rmi.UnexpectedException
+import java.util.*
 import kotlin.math.absoluteValue
+
+// TODO: Factor randState out of here and into parameters for FP usage.
+private fun randomMPZ(max: MPZ): MPZ {
+    val randState = RandState.randinitMt()
+    return MPZ.urandomm(randState, max)
+}
+
+// ***************
+// *** HELPERS ***
+// ***************
+fun <T> Optional<T>.toKotlinNullable(): T? = orElse(null)
+
 
 // *****************
 // *** CONSTANTS ***
 // *****************
 
 val MPZ_ZERO: MPZ = MPZ(0L)
+val MPZ_ONE: MPZ = MPZ(1L)
+
+enum class Legendre(val value: Int) {
+    DIVIDES(0),
+    NOT_RESIDUE(-1),
+    RESIDUE(1);
+
+    companion object {
+        fun fromValue(value: Int): Legendre =
+            entries.firstOrNull { it.value == value }
+                ?:throw IllegalArgumentException("Invalid Legendre value: $value.")
+    }
+}
 
 // ******************
 // *** CONVERTERS ***
 // ******************
+
+fun MPZ(i: Int): MPZ =
+    MPZ(i.toLong())
 
 fun Int.toMPZ(): MPZ =
     MPZ(this.toLong())
@@ -40,6 +71,10 @@ operator fun Long.times(mpz: MPZ): MPZ =
     mpz.mul(this)
 
 
+operator fun MPZ.plus(value: Int): MPZ =
+    if (value < 0) subUi(value.toLong().absoluteValue)
+    else addUi(value.toLong())
+
 operator fun MPZ.plus(value: Long): MPZ =
     if (value < 0) subUi(value.absoluteValue)
     else addUi(value)
@@ -48,12 +83,20 @@ operator fun MPZ.minus(value: Long): MPZ =
     if (value < 0) addUi(value.absoluteValue)
     else subUi(value)
 
+operator fun MPZ.div(value: Int): MPZ =
+    this / value.toLong()
+
+operator fun MPZ.div(value: Long): MPZ =
+    if (value == 0L) throw ArithmeticException("Cannot divide $this by 0.")
+    else div(value)
+
+
 // *************************
 // *** FINITE FIELDS Z_p ***
 // *************************
 class Zn(val modulus: MPZ) {
     // Equivalent to 0 until modulus.
-    val mod_range = MPZ_ZERO..(modulus - 1)
+    private val modRange = MPZ_ZERO.rangeUntil(modulus)
 
     init {
         require(modulus > MPZ_ZERO) { "Modulus must be greater than 0" }
@@ -61,12 +104,9 @@ class Zn(val modulus: MPZ) {
         require(primalityStatus == PrimalityStatus.PRIME || primalityStatus == PrimalityStatus.PROBABLY_PRIME)
     }
 
-    fun pull(value: MPZ): EZn =
-        EZn(value, this)
-
-    inner class EZn(val value: MPZ, private val ring: Zn) {
+    inner class EZn(val value: MPZ, private val ring: Zn = this) {
         init {
-            require(value in mod_range) { "Value must be in the range [0, modulus)" }
+            require(value in modRange) { "Value must be in the range [0, modulus)" }
         }
 
         private fun perform(op: (MPZ, MPZ) -> MPZ, other: EZn): EZn {
@@ -84,9 +124,96 @@ class Zn(val modulus: MPZ) {
             perform(MPZ::mul, other)
 
         operator fun div(other: EZn): EZn {
-            val otherInv = EZn(other.value.invert(modulus).orElse(null) ?:
+            val otherInv = EZn(other.value.invert(modulus).toKotlinNullable() ?:
                 throw ArithmeticException("$this has no multiplicative inverse."), ring)
             return perform(MPZ::mul, otherInv)
+        }
+
+        operator fun unaryMinus(): EZn =
+            EZn(value.neg().mod(modulus), ring)
+
+        val invert: EZn? by lazy {
+            value.invert(modulus).toKotlinNullable()?.let { EZn(it, ring) }
+        }
+
+        fun pow(n: MPZ): EZn =
+            EZn(value.powm(n, modulus), ring)
+
+        fun pow(n: Long): EZn {
+            if (n == 0L)
+                return EZn(MPZ_ONE, ring)
+
+            // If n < 0, we must invert; otherwise we just proceed.
+            val (a, np) = when {
+                n < 0 -> invert?.let {Pair(invert!!.value, -n) } ?: throw ArithmeticException("$value has no inverse (mod $modulus).")
+                else -> Pair(value, n)
+            }
+            return EZn(a.powmUi(np, modulus), ring)
+        }
+
+        // Calculate the Legendre symbol, (a/p):
+        // If a non-residue class, exit immediately.
+        val legendre: Legendre by lazy {
+            Legendre.fromValue(value.legendre(modulus))
+        }
+
+        // Calculate the square root mod the modulus.
+        val sqrt: EZn? by lazy {
+            // Must be a quadratic residue to have an inverse: if not, terminate.
+            if (legendre != Legendre.RESIDUE)
+                return@lazy null
+
+            // Check if p = 3 (mod 4), this is an easy computation, namely:
+            // a^((p + 1)/4) (mod p).
+            if (modulus.tstbit(0) == 1 && modulus.tstbit(1) == 1)
+                return@lazy pow((modulus + 1).divexact(4.toMPZ()))
+
+            // Otherwise, we must use Tonelli and Shanks.
+            // Initialize q to n - 1 (even), find # of zeros on right in binary representation, and eliminate them.
+            val mm1 = modulus - 1
+            val e = mm1.scan1(0)
+            val q = mm1.tdivq2Exp(e)
+
+            // Find a generator. Randomly search for a non-residue.
+            tailrec fun findGenerator(n: EZn = EZn(MPZ_ONE, ring)): EZn =
+                if (n.legendre != Legendre.NOT_RESIDUE)
+                    findGenerator(EZn(randomMPZ(modulus), ring))
+                else n
+            val generator = findGenerator()
+
+            // Initialize the working components.
+            // y = n^q, where q is an MPZ, so the power is calculated with the modulus.
+            val y = generator.pow(q)
+            var r = e
+
+            // x = a^{(q-1)/2} as q-1 should be exactly divisible by 2 now.
+            var x = pow((q - 1) / 2)
+            var b = this * x * x
+            x = this * x
+
+            // Loop on algorithm until finished or failure.
+            // Terminate when b == 1.
+            while (b.value != MPZ_ONE) {
+                var m = 1L
+                var t1 = b
+                while (m < r) {
+                    t1 *= t1
+                    if (t1.value == MPZ_ONE)
+                        break
+                    ++m
+                }
+
+                // This should never happen as a is a quadratic residue.
+                if (r == m)
+                    throw UnexpectedException("Not a quadratic residue: $this")
+
+                val t = y.pow(1L shl (r -m - 1).toInt())
+                r = m
+                x *= t
+                b *= y
+            }
+
+            return@lazy x
         }
 
         override fun toString(): String = "$value (mod $modulus}"
